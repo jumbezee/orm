7c10cb3d573f1b09cb4289b07b6f5b1ed1fafd18
Artoym Pechoyrin <pecha7x@gmail.com>
2016-07-15 21:44:42 +0300

    simple migration feature

--- orm_kolya.rb
+++ orm_kolya.rb
@@ -1,52 +1,61 @@
 require 'pg'
 
-class Base
-    # NATIVE_DATABASE_TYPES = {
-    #     text:        { name: "text" }
-    # }
-    def initialize
-      @conn = PG::Connection.new( :dbname => 'task')
-    end
+module TableImage
 
+  class SimpleTable
+    COLUMN_TYPES = [ :boolean, :integer, :text ]
 
-      def create_table(table_name, &block)
-        yield
+    attr_reader :table_query, :table_name
 
-
-          # create = "CREATE TABLE #{table_name} (#{a});"
-          # @conn.exec(create)
-
-
-        # create = "CREATE TABLE #{table_name} ();"
-        # @conn.exec(create)
+    COLUMN_TYPES.each do |type|
+      define_method "#{type}" do |attr|
+        table_query << "#{attr} #{type},"
       end
     end
 
+    def initialize(table_name)
+      @table_name = table_name
+      @table_query = "CREATE TABLE #{table_name} ("
+    end
+
+    def full_table_query
+      drop_table_query! + "#{table_query.chomp(',')});"
+    end
+
+    def drop_table_query!
+      "DROP TABLE IF EXISTS #{table_name};"
+    end
+  end
+
+end
 
 
-# def hashhhh(hh)
-#   @arr = []
-#   @arr1 = []
-#   @arr2 = []
-#   hh.each do |line|
-#     @arr<<line[0]
-#     @arr1<<line[1]
-#     @arr = "#{@arr1} #{@arr2}"
-#   end
-#   print @arr
-# end
-#
-#
-# hashhhh(name: varchar(80) ,age: integer)
+class Base
+  include TableImage
 
+  DATABASE_NAME = 'kolya_db'
 
+  def initialize
+    @conn = PG::Connection.new(:dbname => DATABASE_NAME)
+  end
+
+  def create_table(table_name)
+    new_table = SimpleTable.new(table_name)
+    yield new_table
+
+    @conn.exec(new_table.full_table_query)
+  end
+end
 
 
 class CreateCars < Base
   def change
     create_table :cars do |t|
+      t.integer :max_speed
+      t.text :description
       t.text :name
       t.text :model
+      t.boolean :is_stock
     end
   end
 
